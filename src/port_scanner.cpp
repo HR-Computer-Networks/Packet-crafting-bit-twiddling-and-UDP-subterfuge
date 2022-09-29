c87802754bcc469cd824cf1a915da934c15d3347
meloncrush <matteobecatti7@gmail.com>
2022-09-29 17:09:46 +0000

    Fixed port_scanner, ready. Added readme

--- src/port_scanner.cpp
+++ src/port_scanner.cpp
@@ -1,10 +1,3 @@
-// string& passes by reference to a function: the actual memory store
-// string passes by value to a function
-
-// int* n1 creates a pointer variable (will store the address of another var)
-// int n2 creates a regular variable
-// n1 = &n2 passes the memory store / address of n2 to n1
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -22,29 +15,23 @@ struct sockaddr_in serv_addr;
 char buffer[1024];
 
 bool port_is_open(int portno) {
-
-
-    // sets target port number to big-endian storage
+    // Sets target port number to big-endian storage
 	serv_addr.sin_port = htons( portno );
 
-    // create a socket data structure
-    int sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // UDP
+    // Create the UDP socket
+    int sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-    // send a message via UDP to watch for a response
+    // Send a message via UDP to watch for a response
     for (int i=0; i<3; i++) {
 	    int res = sendto(sock_fd, "test", sizeof("test")-1, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     }
-
-    socklen_t length = sizeof(serv_addr);
-    // receive from
-    // int nread = recvfrom(sock_fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&serv_addr, &length);
-    // printf("from %s,%d: %s\n", inet_ntoa(serv_addr.sin_addr), serv_addr.sin_port, buffer);
 
     int recVal = 0;
     fd_set rfds;
     FD_ZERO(&rfds);
     FD_SET(sock_fd, &rfds);
 
+    // Delay
     struct timeval tv;
     tv.tv_usec = 0;
     tv.tv_sec = 1.0;
@@ -54,50 +41,39 @@ bool port_is_open(int portno) {
         return false;
     }
     else if (recVal == -1) {
-        cout << "Error" << endl;
         return false;
     }
     else {
-        cout << portno << endl;
-        if(FD_ISSET(sock_fd, &rfds)){ 
-            int n = read(sock_fd, buffer, 256);
-            cout << buffer << endl;
-        }
-        memset(buffer, 0, 1024);
-        fflush(stdout);
         return true;
     }
 
 }
 
 int main(int argc, char **argv) {
-	// should be given 2 arguments exactly: IP address, port
-	// all other arguments ignored
-	if(argc<4) { 
-		printf("usage: server <serverip> <low_port> <high_port>\n"); 
+    // Read the parameters passed from terminal
+	if(argc < 4) { 
+		printf("Usage: server <serverip> <low_port> <high_port>\n"); 
 		exit(1);
 	}
 
-    int sock = 0;
-
-    // set block of memory
+    // Set block of memory
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET ;
 
-    // only works with ip address, not with hostname
+    // Assigns the IP and the ports passed as parameters from the terminal
 	char *server_ip = argv[1];
 	int low_portno = atoi(argv[2]);
     int high_portno = atoi(argv[3]);
 
-    // Need to know the IP address of the server we are connecting t
+    // Need to know the IP address of the server we are connecting to,
 	// stores the IP address in binary form in serv_addr.sin_addr
 	if( inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
 		perror(" failed to set socket address");
 		exit(0);
 	}
 
-    cout << "Open ports:" << endl;
+    // Checks which ports are open inside the selected range
     for (int i=low_portno; i<=high_portno; i++) {
-        port_is_open(i);
+        printf("Port %d is %d \n", i, port_is_open(i));
     }
 }
